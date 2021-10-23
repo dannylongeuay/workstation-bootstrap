0035bf5fd16d0820e2a641f594cf51ad33a9a34c
dannylongeuay <dannylongeuay@yahoo.com>
2021-10-23 00:33:15 -0700

    feat: added spaceship theme

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash || true
+
+ZSH_CUSTOM_THEMES="$HOME/.oh-my-zsh/custom/themes"
+ZSH_SPACESHIP_THEME="$ZSH_CUSTOM_THEMES/spaceship.zsh-theme"
+SPACESHIP_PROMPT="$ZSH_CUSTOM_THEMES/spaceship-prompt"
+SPACESHIP_THEME="$SPACESHIP_PROMPT/spaceship.zsh-theme"
+
+if [ ! -d $SPACESHIP_PROMPT ]
+then
+    git clone https://github.com/spaceship-prompt/spaceship-prompt.git $SPACESHIP_PROMPT --depth=1
+fi
+
+if [ ! -f $ZSH_SPACESHIP_THEME ]
+then
+    ln -s $SPACESHIP_THEME $ZSH_SPACESHIP_THEME
+fi
--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ sudo apt update
 sudo apt install -y curl git make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-zsh
-
-# Install asdf
-/bin/bash scripts/asdf.sh
+zsh fonts-powerline ttf-ancient-fonts
 
 # Configure ZSH
 /bin/bash scripts/zsh.sh
+
+# Install asdf
+/bin/bash scripts/asdf.sh
 
 # Copy dotfiles
 /bin/bash scripts/dotfiles.sh
