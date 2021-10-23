2f66b82a0cd36d4a4038dd6ef189eda6e82445f0
dannylongeuay <dannylongeuay@yahoo.com>
2021-10-23 01:48:05 -0700

    feat: install docker

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ sudo apt update
 sudo apt install -y curl git make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-zsh fonts-powerline ttf-ancient-fonts coreutils
+zsh fonts-powerline ttf-ancient-fonts coreutils ca-certificates gnupg lsb-release
+
+# Install docker
+/bin/bash scripts/docker.sh
 
 # Configure ZSH
 /bin/bash scripts/zsh.sh
