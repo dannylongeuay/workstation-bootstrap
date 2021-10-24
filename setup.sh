d118708baf15ba960056c25a9712a1606b10d4dc
dannylongeuay <dannylongeuay@yahoo.com>
2021-10-24 11:35:53 -0700

    feat: added packages and alias

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ sudo apt update
 sudo apt install -y curl git make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-zsh fonts-powerline ttf-ancient-fonts coreutils ca-certificates gnupg lsb-release
+zsh fonts-powerline ttf-ancient-fonts coreutils ca-certificates gnupg lsb-release \
+whois dnsutils
 
 # Install docker
 /bin/bash scripts/docker.sh
