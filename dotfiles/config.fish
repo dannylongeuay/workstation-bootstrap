d9215d5190e2bd57ddb3eac395aadaa4047286b2
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-12-03 22:41:44 -0800

    feat: :sparkles: add fish abbreviations

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,6 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     starship init fish | source
     source ~/.asdf/asdf.fish
     doctl completion fish | source
+    abbr --add --global gs git status
+    abbr --add --global gc git commit -m
+    abbr --add --global gd git diff
+    abbr --add --global gl "git log --oneline"
+    abbr --add --global ga git add -A
+    abbr --add --global g git
+    abbr --add --global ka kubectl get all -A
+    abbr --add --global k kubectl
 end
