b92b66a185ecff4f137bee040488309311e85b8b
Daniel Longeuay <dannylongeuay@yahoo.com>
2021-12-03 23:19:11 -0800

    feature: :sparkles: add czrc

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,6 +3,6 @@
 printf "\n##### Installing Dotfiles #####\n"
 
 cp dotfiles/bashrc ~/.bashrc
-# cp dotfiles/zshrc ~/.zshrc
+cp dotfiles/czrc ~/.czrc
 cp dotfiles/tmux.conf ~/.tmux.conf
 cp dotfiles/config.fish ~/.config/fish/config.fish
