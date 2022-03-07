af869a59cd7b9df8170444bbbbd32f9fffd90bc7
dannylongeuay <dannylongeuay@yahoo.com>
2022-03-06 23:31:09 -0800

    fix: :bug: no lsp installation in headless mode

--- dotfiles/nvim/lsp/lsp-installer.lua
+++ dotfiles/nvim/lsp/lsp-installer.lua
@@ -2,25 +2,30 @@ local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
 	return
 end
--- Include the servers you want to have installed by default below
-local servers = {
-	"bashls",
-	"gopls",
-	"jsonls",
-	"ltex",
-	"pyright",
-	"sumneko_lua",
-	"svelte",
-	"tailwindcss",
-	"tsserver",
-	"yamlls",
-}
 
-for _, name in pairs(servers) do
-	local server_is_found, server = lsp_installer.get_server(name)
-	if server_is_found and not server:is_installed() then
-		print("Installing " .. name)
-		server:install()
+local uis = vim.api.nvim_list_uis()
+
+if #uis > 0 then
+	-- Include the servers you want to have installed by default below
+	local servers = {
+		"bashls",
+		"gopls",
+		"jsonls",
+		"ltex",
+		"pyright",
+		"sumneko_lua",
+		"svelte",
+		"tailwindcss",
+		"tsserver",
+		"yamlls",
+	}
+
+	for _, name in pairs(servers) do
+		local server_is_found, server = lsp_installer.get_server(name)
+		if server_is_found and not server:is_installed() then
+			print("Installing " .. name)
+			server:install()
+		end
 	end
 end
 
