f6d9b664fc699e234074cfbbe5e3acc354660daf
dannylongeuay <dannylongeuay@yahoo.com>
2022-03-15 22:49:30 -0700

    feat: :sparkles: add neovim lua api completions

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -102,6 +102,7 @@ cmp.setup({
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
+				nvim_lua = "[LUA]",
 				luasnip = "[Snippet]",
 				buffer = "[Buffer]",
 				path = "[Path]",
@@ -111,6 +112,7 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -78,6 +78,7 @@ return packer.startup(function(use)
 	use("hrsh7th/cmp-cmdline") -- cmdline completions
 	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 	use("hrsh7th/cmp-nvim-lsp") -- lsp completions
+	use("hrsh7th/cmp-nvim-lua") -- neovim lua api completions
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
