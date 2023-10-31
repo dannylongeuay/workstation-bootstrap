28ba8fea6cf39a2fa473feae99f14a19a4f4f933
dannylongeuay <dannylongeuay@yahoo.com>
2023-10-31 15:16:41 -0700

    feat(fish): :sparkles: add git commit functions

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -33,7 +33,6 @@ if status is-interactive
   abbr --add --global g git
   abbr --add --global ka kubectl get all -A
   abbr --add --global k kubectl
-  abbr --add --global nv nvim
   abbr --add --global tl "tree | less"
   abbr --add --global tmd "tmux new -s dev"
   abbr --add --global tma tmux a
@@ -128,3 +127,28 @@ function fish_title
   echo (basename (prompt_pwd)) \| $argv;
 end
 
+function _gcm
+  argparse --name=_gcm 'b/breaking' -- $argv
+  set -l breaking ""
+  set -l cmd
+  if set -q _flag_breaking
+    set breaking '!'
+  end
+  switch (count $argv)
+    case '4'
+      set cmd git commit -m "\"$argv[1]($argv[3])$breaking: $argv[2] $argv[4..-1]\""
+    case '3'
+      set cmd git commit -m "\"$argv[1]$breaking: $argv[2] $argv[3..-1]\""
+    case '*'
+      set cmd echo "Incorrect number of arguments"
+  end
+  eval $cmd
+end
+
+function gfeat; _gcm "feat" ":sparkles:" $argv; end;
+function gfix; _gcm "fix" ":bug:" $argv; end;
+function gperf; _gcm "perf" ":zap:" $argv; end;
+function gchore; _gcm "chore" ":wrench:" $argv; end;
+function grefactor; _gcm "refactor" ":recycle:" $argv; end;
+function gstyle; _gcm "style" ":art:" $argv; end;
+function gdocs; _gcm "docs" ":pencil:" $argv; end;
