a320e4dba3cb1b415e9f24026e0d2f5f18e173f8
dannylongeuay <dannylongeuay@yahoo.com>
2024-04-15 13:33:02 -0700

    chore(wezterm): :wrench: disable alt+enter keybind

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -4,5 +4,12 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Catppuccin Mocha'
 config.audible_bell = 'Disabled'
 config.enable_tab_bar = false
+config.keys = {
+  {
+    key = 'Enter',
+    mods = 'ALT',
+    action = wezterm.action.DisableDefaultAssignment,
+  }
+}
 
 return config
